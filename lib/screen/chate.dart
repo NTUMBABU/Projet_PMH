e11f25450c5218a7d03d366f1a40931233a3d615
NTUMBABU <tshiteyababci@gmail.com>
2023-05-10 16:52:05 +0400

    creation de la premier parti du chate

--- lib/screen/chate.dart
+++ lib/screen/chate.dart
@@ -10,11 +10,32 @@ class Chate extends StatefulWidget {
 class _ChateState extends State<Chate> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Icon(
-      Icons.ac_unit_rounded,
-      size: 64.0,
-      color: Colors.black,
-    ));
+    
+    const numItems = 20;
+    const _biggerFont = TextStyle(fontSize: 18.0);
+
+    Widget _buildRow(int idx){
+      return ListTile(
+        leading: CircleAvatar(child: Text('$idx'),),
+        title: Text('Item $idx', style: _biggerFont),
+        trailing: Icon(Icons.dashboard),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FedBack', style: TextStyle(color: Color(0xFFECEFF1))),
+        backgroundColor: const Color(0xFF90A4AE),
+      ),
+      body: ListView.builder(
+        itemCount: numItems * 2,
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext context, int i){
+          if(i.isOdd) return const Divider();
+          final index = i ~/ 2 + 1;
+          return _buildRow(index);
+        },
+      )
+    );
   }
 }
